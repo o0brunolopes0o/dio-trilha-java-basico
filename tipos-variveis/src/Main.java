94522aff32c2e91fb8d3152b49c8adcf5e7d9043
Bruno Lopes <44236749+o0brunolopes0o@users.noreply.github.com>
2024-08-04 16:02:57 -0300

    operadores

--- tipos-variveis/src/Main.java
+++ tipos-variveis/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         final double VALOR_DE_PI = 3.14; //representação de final const
         System.out.println(VALOR_DE_PI);
-        
+
         byte b = 127;
         short s = 32767;
         int i = 2147483647;
