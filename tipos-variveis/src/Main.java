7447adaff7fe3bfe3b7cab10f602de324f46de11
Bruno Lopes <44236749+o0brunolopes0o@users.noreply.github.com>
2024-08-04 15:59:52 -0300

    tipos-primitivos

--- tipos-variveis/src/Main.java
+++ tipos-variveis/src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ola mundo!");
+        final double VALOR_DE_PI = 3.14; //representação de final const
+        System.out.println(VALOR_DE_PI);
+        
+        byte b = 127;
+        short s = 32767;
+        int i = 2147483647;
+        long l = 9223372036854775807L;
+        float f = 3.4028235E38f;
+        double d = 1.7976931348623157E308;
+        char c = 'a';
+        boolean bool = true;
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(bool);
     }
 }
